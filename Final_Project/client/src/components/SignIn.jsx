101797002197e47e90df79eae10ae31fb958f664
Shems <96719853+IsShems@users.noreply.github.com>
2024-07-09 00:00:30 +0400

    Update SignIn.jsx

--- Final_Project/client/src/components/SignIn.jsx
+++ Final_Project/client/src/components/SignIn.jsx
@@ -3,14 +3,14 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
-    const [credentials, setCredentials] = useState({ username: '', password: '' })
-    const [errorMessage, setErrorMessage] = useState('')
-    const navigate = useNavigate()
+    const [formData, setFormData] = useState({ username: '', password: '' });
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
     
     const handleChange = (e) => {
-        const { name, value } = e.target
-        setCredentials({
-            ...credentials,
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
             [name]: value,
         });
     };
@@ -18,14 +18,14 @@ export default function SignIn() {
     const handleSignIn = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:3000/signin', credentials);
-            const { token } = response.data
+            const response = await axios.post('http://localhost:3000/signin', formData);
+            const { token } = response.data;
             if (token) {
-                sessionStorage.setItem('token', token)
-                navigate("/chat/Sahib")
+                sessionStorage.setItem('token', token);
+                navigate("/chat/Sasha");
             }
         } catch (error) {
-            setErrorMessage('Login failed:( Please check your username and password!');
+            setError('Login failed:( Please check your username and password!');
         }
     };
 
@@ -33,7 +33,7 @@ export default function SignIn() {
         <div className="container" >
             <div className="form-container">
                 <h2>Sign In</h2>
-                {errorMessage && <p className="error">{errorMessage}</p>}
+                {error && <p className="error">{error}</p>}
                 <form onSubmit={handleSignIn}>
                     <div className="form-group-div">
                         <div className="form-group">
@@ -41,7 +41,7 @@ export default function SignIn() {
                             <input
                                 type="text"
                                 name="username"
-                                value={credentials.username}
+                                value={formData.username}
                                 onChange={handleChange}
                                 required
                             />
@@ -51,7 +51,7 @@ export default function SignIn() {
                             <input
                                 type="password"
                                 name="password"
-                                value={credentials.password}
+                                value={formData.password}
                                 onChange={handleChange}
                                 required
                             />
@@ -59,7 +59,7 @@ export default function SignIn() {
                         <button type="submit">Sign In</button>
                         <div className='navigate-container'>
                             <p>
-                                Don't have an account?{' '}
+                                Do not have an account?{' '}
                                 <Link to='/signUp' className='navigate-text'>
                                     Sign Up
                                 </Link>
