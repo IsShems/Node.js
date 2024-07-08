543997eef7a7201df30432316e45b35c6ef576c8
Shems <96719853+IsShems@users.noreply.github.com>
2024-07-08 23:58:10 +0400

    Update SignIn.jsx

--- Final_Project/client/src/components/SignIn.jsx
+++ Final_Project/client/src/components/SignIn.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
     const [credentials, setCredentials] = useState({ username: '', password: '' })
-    const [error, setError] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
     const navigate = useNavigate()
     
     const handleChange = (e) => {
@@ -25,50 +25,49 @@ export default function SignIn() {
                 navigate("/chat/Sahib")
             }
         } catch (error) {
-            setError('Login failed. Please check your username and password.');
+            setErrorMessage('Login failed:( Please check your username and password!');
         }
     };
 
     return (
         <div className="container" >
-        <div className="form-container">
-            <h2>Sign In</h2>
-            {error && <p className="error">{error}</p>}
-            <form onSubmit={handleSignIn}>
-            <div className="form-group-div">
-                <div className="form-group">
-                    <label>Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        value={credentials.username}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={credentials.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Sign In</button>
-                <div className='navigate-container'>
-                    <p>
-                        Do not have an account?{' '}
-                        <Link to='/signUp' className='navigate-text'>
-                            Sign Up
-                        </Link>
-                    </p>
+            <div className="form-container">
+                <h2>Sign In</h2>
+                {errorMessage && <p className="error">{errorMessage}</p>}
+                <form onSubmit={handleSignIn}>
+                    <div className="form-group-div">
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input
+                                type="text"
+                                name="username"
+                                value={credentials.username}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                value={credentials.password}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        <button type="submit">Sign In</button>
+                        <div className='navigate-container'>
+                            <p>
+                                Don't have an account?{' '}
+                                <Link to='/signUp' className='navigate-text'>
+                                    Sign Up
+                                </Link>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
-        </div>
-
     );
 }
